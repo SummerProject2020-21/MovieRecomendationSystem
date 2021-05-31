46d89ee5ff44fcf44bcbf35cf36558412023b78b
Prerit Kumar Jha <preritkrjha@gmail.com>
2021-05-31 14:37:27 +0530

    corousal added

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,5 +1,10 @@
 import React from "react";
+import ImgSlider from "./ImgSlider";
 
 export default function Trending() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <ImgSlider />
+    </>
+  );
 }
