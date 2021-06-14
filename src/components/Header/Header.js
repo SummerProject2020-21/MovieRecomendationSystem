b36f0e25d68ba9fd83814bbc2f0fa995edd6e9de
Prerit Kumar Jha <preritkrjha@gmail.com>
2021-06-14 18:20:00 +0530

    dicuss

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,10 @@
+import React, { useEffect } from "react";
 import "./Header.css";
 import Fade from "react-reveal/Fade";
 import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router";
 const Header = () => {
+  const history = useHistory();
   return (
     <div
       style={{
@@ -16,25 +19,6 @@ const Header = () => {
         <span className="header">
           <Fade top> 🎬 Movie Recommendation System</Fade>
         </span>
-      </div>
-      <div
-        style={{
-          width: "20%",
-          float: "right",
-          zIndex: "10px",
-          backgroundImage: "linear-gradient(#39445a,#455880,#39445a)",
-          justifyContent: "center",
-          paddingTop: "1%",
-        }}
-      >
-        <Button
-          variant="outlined"
-          color="primary"
-          href="/discussion"
-          style={{ color: "#ffffff", borderColor: "#ffffff" }}
-        >
-          DISCUSS
-        </Button>
       </div>
     </div>
   );
