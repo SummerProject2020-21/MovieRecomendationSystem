15d02f95c699cfbe050291c83f516ce1e700dfea
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2021-06-14 17:38:53 +0530

    discussion added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,42 @@
 import "./Header.css";
 import Fade from "react-reveal/Fade";
+import Button from "@material-ui/core/Button";
 const Header = () => {
   return (
-    <span onClick={() => window.scroll(0, 0)} className="header">
-      <Fade top> 🎬 Movie Recommendation System</Fade>
-    </span>
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        boxShadow: "1px 1px 5px black",
+        color: "white",
+        zIndex: "100",
+      }}
+    >
+      <div style={{ width: "100%" }}>
+        <span className="header">
+          <Fade top> 🎬 Movie Recommendation System</Fade>
+        </span>
+      </div>
+      <div
+        style={{
+          width: "20%",
+          float: "right",
+          zIndex: "10px",
+          backgroundImage: "linear-gradient(#39445a,#455880,#39445a)",
+          justifyContent: "center",
+          paddingTop: "1%",
+        }}
+      >
+        <Button
+          variant="outlined"
+          color="primary"
+          href="/discussion"
+          style={{ color: "#ffffff", borderColor: "#ffffff" }}
+        >
+          DISCUSS
+        </Button>
+      </div>
+    </div>
   );
 };
 
